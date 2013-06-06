f06ec639930c6cdad7d9124f2471564da44d9eff
Vitrum Mus <VitrumMus@gmail.com>
2013-06-06 18:49:38 -0400

    Setup: setup complete
    A window opens up and is ready to display a 3D environment

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,13 @@
 package main;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.util.glu.GLU.gluPerspective;
+
 /**
  * User: hamdan
  * Date: 2013-06-06
@@ -8,7 +16,48 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
+		new Main();
+	}
 
+	public Main() {
+		initDisplay();
+		initGL();
+		programLoop();
+	}
+
+	private void initDisplay() {
+		// create the display
+		try {
+			Display.setDisplayMode(new DisplayMode(800, 600));
+			Display.setTitle("Star field");
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			Display.destroy();
+			System.exit(1);
+		}
+	}
+
+	private void initGL() {
+		// edit projection matrix
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		gluPerspective(70.0f, 800.0f / 600.0f, 0.001f, 100.0f);
+		// go back to model matrix
+		glMatrixMode(GL_MODELVIEW);
+	}
+
+	private void programLoop() {
+		while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
+			Display.update();
+			Display.sync(60);
+		}
+		exitProgram();
+	}
+
+	private void exitProgram() {
+		Display.destroy();
+		System.exit(0);
 	}
 
 }
